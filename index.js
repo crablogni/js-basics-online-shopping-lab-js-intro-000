0a3f79f5b240a9ddc0dc628d40e7457dfe425b37
hector ariceaga <hector_ariceaga@hotmail.com>
2018-03-09 18:07:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-var card= false
-  if ()
+  if (cardNumber === null){
+    console.log (`Sorry, we don't have a credit card on file for you.`)
+  }
 }
