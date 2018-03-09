eff31a18aeb62437c25af8663acb8f3d38af5928
hector ariceaga <hector_ariceaga@hotmail.com>
2018-03-09 18:01:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,14 +52,22 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  var inCart = false
   for (var i = 0; i < cart.length; i++){
+    var inCart = true
     if (cart[i].hasOwnProperty(item)){
       cart = cart.splice(i)
+      console.log(cart)
     }
+  }
+  if (!inCart){
+    console.log(`That item is not in your cart.`)
   }
   return cart
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+var card= false
+  if ()
 }
