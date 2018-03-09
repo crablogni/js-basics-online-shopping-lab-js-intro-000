f317e7f65714c19e039e29dd5af817af3d87f017
hector ariceaga <hector_ariceaga@hotmail.com>
2018-03-09 18:09:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
 
   for (var i = 0; i < cart.length; i++){
     if (cart[i].hasOwnProperty(item)){
-      cart = cart.splice(i).concat(cart.splice(i+1))
+      cart = cart.slice(i).concat(cart.slice(i+1))
       var inCart = true
       console.log(cart)
     }
